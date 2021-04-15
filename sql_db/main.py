0e90838df204c9166b96e795a88e3d674ad223a6
Wojciech Kołodziejak <wojtekk61@gmail.com>
2021-04-15 11:52:10 +0200

    Add logging feature

--- sql_db/main.py
+++ sql_db/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
-
-from database import database, metadata, engine
-
-from endpoints import router
-
+from .database import database, metadata, engine
+from .endpoints import router
 import uvicorn
+from fastapi.logger import logger as fastapi_logger
+import logging
+
 app = FastAPI()
+
+gunicorn_error_logger = logging.getLogger("gunicorn.error")
+gunicorn_logger = logging.getLogger("gunicorn")
+uvicorn_access_logger = logging.getLogger("uvicorn.access")
+fastapi_logger.handlers = gunicorn_error_logger.handlers
 
 metadata.create_all(engine)
 
@@ -24,3 +29,6 @@ app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    fastapi_logger.setLevel(gunicorn_logger.level)
+else:
+    fastapi_logger.setLevel(logging.DEBUG)
